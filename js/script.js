fc10d0c9e16fd39d2fb9ee40addc5d116dfbfb54
rus733 <9282716617@mail.ru>
2021-12-20 19:09:13 +0300

    первый класс first создан

--- js/script.js
+++ js/script.js
@@ -6,3 +6,14 @@ class First {
     console.log('Привет я метод родителя!');
   }
 }
+class Second extends First {
+  hello() {
+    super.hello();
+
+    console.log('А я наследуемый метод!');
+  }
+}
+
+const second = new Second();
+
+second.hello();
